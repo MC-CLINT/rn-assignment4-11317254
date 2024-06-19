0c2754988a0ae23f382034c24da0f4444999312c
MC-CLINT <ckvidam@st.ug.edu.gh>
2024-06-19 14:58:17 +0000

    Working on HomeScreen

--- MyApp/HomeScreen.js
+++ MyApp/HomeScreen.js
@@ -1,7 +1,8 @@
 
 import React from "react";
-import { SafeAreaView, FlatList, ScrollView, StyleSheet, Text, Image } from "react-native";
+import { SafeAreaView, FlatList, ScrollView, StyleSheet, Text, View, TextInput, Pressable } from "react-native";
 import FeaturedCard from "./components/FeaturedCard";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function HomeScreen(){
 
@@ -65,7 +66,7 @@ export default function HomeScreen(){
         {
             id:'7',
             backgroundColor: '#356899',
-            title: 'Machine Learning Engineer',
+            title: 'Project Engineer',
             subtitle: 'Oracle',
             icon: require('./assets/oracle.png'),
             amount: '$ 210,000',
@@ -85,7 +86,22 @@ export default function HomeScreen(){
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView>
+            <View style={styles.topBar}>
+            <View style= {styles.searchArea}>
+                <TextInput style={styles.searchText} 
+                placeholder='Search a job or position' />
+                <Pressable style={styles.searchButton}>
+                <Icon style={styles.searchIcon} name="search" size={20} color="black" />
+                </Pressable>
+            </View>
+
+            <View style={styles.barsIconBox}>
+            <Icon style={styles.barsIcon} name="bars" size={30} color="black" />
+            </View>
+                
+            </View>
             <Text style={styles.text}>Featured Jobs</Text>
+            <View style={styles.FlatList}>
             <FlatList 
             data={cardData}
             horizontal
@@ -104,7 +120,7 @@ export default function HomeScreen(){
             
             />
 
-
+</View>
 
 
             </ScrollView>
@@ -122,10 +138,61 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       backgroundColor: '#F4FDFF',
     },
-    text: {
-      fontSize: 12,
-      bottom: 300,
+    topBar: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 20,
+      top: 70,
+      backgroundColor: '#F4FDFF',
+    },
+    searchArea: {
+      flexDirection: 'row',
+      backgroundColor: '#F2F2F3',
+      borderRadius: 10,
+      width: '80%',
+      height: 40,
+      alignItems: 'center',
+    },
+    searchText: {
+      flex: 1,
+      fontSize: 18,
       color: 'black',
+      left: 40,
+
+    },
+    searchButton: {
+      backgroundColor: '#F2F2F3',
+      width: 40,
+      height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        right: 235,
+      borderRadius: 10,
+    },
+    barsIconBox: {
+        backgroundColor: '#F2F2F3',
+        width: 40,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        },
+    text: {
+      fontSize: 18,
+      top: 80,
+      left: 20,
+      color: 'black',
+      fontWeight: 'bold',
+    },
+    FlatList: {
+      flex: 1,
+      height: 300,
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 90,
+      marginLeft: 10,
+
     },
 
 })
